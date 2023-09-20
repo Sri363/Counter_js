964cd3da3ed13499112869834d87de72154307cf
Sri hatake <96491256+Sri363@users.noreply.github.com>
2023-09-20 15:59:10 +0530

    final update

--- app.js
+++ app.js
@@ -9,26 +9,29 @@ const value = document.querySelector('.value');
 
 navBrand.style.textTransform = "uppercase";
 navBrand.style.fontWeight = "900";
-decBtn.addEventListener("click",decrement);
-resBtn.addEventListener("click",reset);
-incBtn.addEventListener("click",increment);
+decBtn.addEventListener("click", decrement);
+resBtn.addEventListener("click", reset);
+incBtn.addEventListener("click", increment);
 
 
 
 let i = 0;
-function increment(){
-    i +=1;
+function increment() {
+    i += 1;
     value.textContent = i;
+    value.style.color = "#08A045";
 }
 
 function decrement() {
     i -= 1;
     value.textContent = i;
+    value.style.color = "#F71735";
 }
 
 function reset() {
     i = 0;
     value.textContent = i;
+    value.style.color = "black";
 }
 
 
