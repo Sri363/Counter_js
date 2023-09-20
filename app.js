f9020656a440aec76a3d86bf61380234e9157cc8
Sri hatake <96491256+Sri363@users.noreply.github.com>
2023-09-20 16:05:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const navBrand = document.querySelector('.nav-brand');
 const decBtn = document.querySelector('#dec');
 const resBtn = document.querySelector('#res');
@@ -9,32 +8,24 @@ const value = document.querySelector('.value');
 
 navBrand.style.textTransform = "uppercase";
 navBrand.style.fontWeight = "900";
-decBtn.addEventListener("click", decrement);
-resBtn.addEventListener("click", reset);
-incBtn.addEventListener("click", increment);
+decBtn.addEventListener("click",decrement);
+resBtn.addEventListener("click",reset);
+incBtn.addEventListener("click",increment);
 
 
 
 let i = 0;
-function increment() {
-    i += 1;
+function increment(){
+    i +=1;
     value.textContent = i;
-    value.style.color = "#08A045";
 }
 
 function decrement() {
     i -= 1;
     value.textContent = i;
-    value.style.color = "#F71735";
 }
 
 function reset() {
     i = 0;
     value.textContent = i;
-    value.style.color = "black";
 }
-
-
-
-
-
